4647f263c6ed135b002fea3437aaee3454c39c2c
WagnerSantos <wagnerstar1@hotmail.com>
2025-10-08 23:44:06 -0300

    Atualização dos arquivos style.css e HomePage.jsx

--- src/pages/Home/HomePage.jsx
+++ src/pages/Home/HomePage.jsx
@@ -1,11 +1,29 @@
-import  "./styles/style.css"
-import Header from "./components/HeaderPage"
+import "./styles/style.css";
+import Header from "./components/HeaderPage";
+import FooterPage from "./components/FooterPage";
 function HomePage() {
-    return (
-        <main>
-            <Header></Header>
-        </main>
-    )
+  return (
+    <main>
+      <Header></Header>
+      <section className="section-main">
+        <div style={{display:"flex",
+        justifyContent: "center",
+        flexDirection: "column",
+        alignItems: "center"
+        }}>
+          <h1>Ola Mundo</h1>
+          <h1>Ola Mundo</h1>
+          <h1>Ola Mundo</h1>
+          <h1>Ola Mundo</h1>
+          <h1>Ola Mundo</h1>
+          <h1>Ola Mundo</h1>
+          <h1>Ola Mundo</h1>
+          <h1>Ola Mundo</h1>
+        </div>
+      </section>
+      <FooterPage></FooterPage>
+    </main>
+  );
 }
 
-export default HomePage
+export default HomePage;
