c696831c32fc51f0b9cc22850cd38f4daa7ded97
Wagner Santos <wagnerstar1@hotmail.com>
2025-10-15 18:23:16 -0300

    90% da paágina do carrinho pronta

--- src/pages/Product/components/ButtonQuantityProduct.jsx
+++ src/pages/Product/components/ButtonQuantityProduct.jsx
@@ -3,7 +3,7 @@ function ButtonQuantityProduct() {
     <div className="flex items-center justify-center gap-4 ">
       <button
        
-        className=" cursor-pointer border-2 border-yellow-500 text-yellow-500 rounded-xl w-8 h-8 flex items-center justify-center text-2xl font-bold hover:bg-yellow-500 hover:text-black transition"
+        className=" cursor-pointer border-2 border-yellow-500 text-yellow-500 rounded-xl w-7 h-7 flex items-center justify-center text-xl font-bold hover:bg-yellow-500 hover:text-black transition"
       >
         −
       </button>
@@ -12,7 +12,7 @@ function ButtonQuantityProduct() {
 
       <button
         
-        className="border-2 cursor-pointer border-yellow-500 text-yellow-500 rounded-xl w-8 h-8 flex items-center justify-center text-2xl font-bold hover:bg-yellow-500 hover:text-black transition"
+        className="border-2 cursor-pointer border-yellow-500 text-yellow-500 rounded-xl w-7 h-7 flex items-center justify-center text-x1 font-bold hover:bg-yellow-500 hover:text-black transition"
       >
         +
       </button>
